978989637da01b95e2bbce0e10e415d2a2dadbce
Vivek Chulani <vivekchulani@gmail.com>
2021-07-02 15:44:52 -0400

    config client

---
+++ src/main/java/com/spring/cloud/config/SpringTestClientApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/cloud/config/controller/SpringConfigClientController.java
@@ -0,0 +1,21 @@
+package com.spring.cloud.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringConfigClientController {
+
+    @Value("${configuration.name}")
+    private String name;
+
+    @Value("${configuration.age}")
+    private String age;
+
+    @GetMapping("/info")
+    public String getUserInfo() {
+        return String.format("Hello %s, You are %s years old", name, age);
+    }
+
+}
