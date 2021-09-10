2d4fffc71146eadf78590f05bd44fc2a5541cc74
Vivek Chulani <vivekchulani@viveks-imac.myfiosgateway.com>
2021-09-09 20:50:34 -0400

    added docker file

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM adoptopenjdk/openjdk11
+
+WORKDIR /app
+
+COPY .mvn/ .mvn
+COPY mvnw pom.xml ./
+
+RUN ./mvnw dependency:go-offline
+
+COPY src ./src
+
+CMD ["./mvnw", "spring-boot:run"]
+
+EXPOSE 8080
--- src/main/java/com/spring/cloud/config/controller/SpringConfigClientController.java
+++ src/main/java/com/spring/cloud/config/controller/SpringConfigClientController.java
@@ -6,18 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
+//@RefreshScope
 public class SpringConfigClientController {
 
-    @Value("${configuration.name}")
-    private String name;
+//    @Value("${configuration.name}")
+//    private String name;
+//
+//    @Value("${configuration.age}")
+//    private String age;
+//
+//    @GetMapping("/info")
+//    public String getUserInfo() {
+//        return String.format("Hello %s, You are %s years old", name, age);
+//    }
 
-    @Value("${configuration.age}")
-    private String age;
-
-    @GetMapping("/info")
-    public String getUserInfo() {
-        return String.format("Hello %s, You are %s years old", name, age);
+    @GetMapping("/dockerize")
+    public String dockerizeApp() {
+        return "I have dockerized this app";
     }
 
 }
